c4bc62e32477071586561907e2aed2e1d851f0c3
artedsolis <artedsolis@gmail.com>
2024-04-19 14:51:23 +0200

    adding data on public and delete function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
+import { useState, useEffect, useCallback } from 'react';
+
 import { BiCalendar, BiArchive } from 'react-icons/bi';
 import Search from './components/Search'; 
 import AddAppointment from './components/AddAppointment';
 import AppointmentInfo from './components/AppointmentInfo';
-import appointmentList from './data.json';
+// import appointmentList from './data.json';
 
 import {  } from 'react-icons/bi';
 
 function App() {
+  // use state for appointmentList
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
     <h1 className="text-5xl">
@@ -19,6 +36,9 @@ function App() {
       {appointmentList.map(appointment => (
           <AppointmentInfo key={appointment.id} 
           appointment={appointment} 
+          onDeleteAppointment={(appointmentId) => {
+            setAppointmentList(appointmentList.filter(appointment => appointment.id !== appointmentId));
+          }}
           />
         ))
        }
